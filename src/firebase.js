c24f1cc451a4169aa02813510c6be3f2471f9f9c
samirmalekdev <samirmalek.dev@gmail.com>
2020-12-19 21:22:21 +0300

    Day - 1

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig =  firebase.initializeApp({
+    apiKey: "AIzaSyCSYvzxxGO1Hx_AdfB0bIXLExEztsONJ3U",
+    authDomain: "clone-fd817.firebaseapp.com",
+    projectId: "clone-fd817",
+    storageBucket: "clone-fd817.appspot.com",
+    messagingSenderId: "431791869532",
+    appId: "1:431791869532:web:8844245f2c1db2cd31e099",
+    measurementId: "G-RDED67ESBK"
+  });
